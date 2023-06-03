addbddb012062e52a7cb38981e4b4b0852f98b5a
dental misorder <denys.bondarenko@nure.ua>
2023-06-03 16:42:14 +0300

    project setup

---
+++ Eremite/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Eremite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            S
+        }
+    }
+}
