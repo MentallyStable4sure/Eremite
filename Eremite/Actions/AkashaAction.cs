817605c791c73de8d721c071396984f58bfa65b9
dental misorder <denys.bondarenko@nure.ua>
2023-06-18 00:10:07 +0300

    !shop, Shop at !akasha page

---
+++ Eremite/Actions/AkashaAction.cs
@@ -0,0 +1,48 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using DSharpPlus;
+using Eremite.Data.DiscordData;
+using DSharpPlus.EventArgs;
+using Eremite.Services;
+using Eremite.Data;
+
+namespace Eremite.Actions
+{
+    public class AkashaAction
+    {
+
+        public static async Task ShowShop(CommandContext context, ComponentInteractionCreateEventArgs args, UserData user, DataHandler dataHandler)
+        {
+            var dropdown = ShopAction.CreateShopDropdown();
+            var messageBuilder = new DiscordMessageBuilder()
+                .AddComponents(dropdown)
+                .WithEmbed(ShopAction.GetEmbedWithShopInfo());
+
+            context.Client.ComponentInteractionCreated += async (sender, args) => await ShopAction.ShopInteracted(user, args, () => SaveDataFromShop(dataHandler, user));
+
+            await args.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage,
+                new DiscordInteractionResponseBuilder(messageBuilder));
+        }
+
+        public static async Task ShowAccountStats(CommandContext context, ComponentInteractionCreateEventArgs args, UserData user)
+        {
+            var embed = StatsAction.GetEmbedWithStats(context.User.AvatarUrl, user);
+
+            await args.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage,
+                    new DiscordInteractionResponseBuilder().AddEmbed(embed));
+        }
+
+        public static async Task ShowCharacterStats(ComponentInteractionCreateEventArgs args, Character character)
+        {
+            await args.Interaction.CreateResponseAsync(
+                    InteractionResponseType.UpdateMessage,
+                    new DiscordInteractionResponseBuilder().AddEmbed(SetCharacterAction.GetEmbedWithCharacterInfo(character)));
+        }
+
+        private async Task SaveDataFromShop(DataHandler dataHandler, UserData user)
+        {
+            var query = new QueryBuilder(user, QueryElement.Wallet, QueryElement.Stats, QueryElement.Characters);
+            await dataHandler.SendData(user, query.BuildUpdateQuery());
+        }
+    }
+}
---
+++ Eremite/Data/DiscordData/DoriLot.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Eremite.Data.DiscordData
+{
+    public enum DoriLot
+    {
+        None = 0,
+        ONE_HUNDRED_PRIMOS = 1,
+        CRIMSON_WITCH_HAT = 2,
+        WELKIN_MOON = 3,
+        ONE_HUNDRED_PILLS = 4
+    }
+}
