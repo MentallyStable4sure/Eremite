0c5770207eb8610fe971118d882f64dcd0ff271d
dental misorder <denys.bondarenko@nure.ua>
2023-06-17 14:37:02 +0300

    Create SacrificeCommand.cs

---
+++ Eremite/Commands/SacrificeCommand.cs
@@ -0,0 +1,51 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Eremite.Actions;
+using Eremite.Services;
+using Eremite.Data;
+using Eremite.Data.DiscordData;
+
+namespace Eremite.Commands
+{
+    public sealed class SacrificeCommand : BaseCommandModule
+    {
+        public DataHandler DataHandler { get; set; }
+
+        [Command("sacrifice"), Description("Sacrifice character for some pills")]
+        public async Task Sacrifice(CommandContext context, string name, string lastname)
+        {
+            var user = await DataHandler.GetData(context.User);
+
+            var matchingCharacter = user
+                .Characters
+            .FirstOrDefault(character =>
+                character.CharacterName.ToLower() == $"{name.ToLower()} {lastname.ToLower()}"
+                || character.CharacterName.ToLower().Contains(name.ToLower()));
+
+            if (matchingCharacter == null)
+            {
+                await context.RespondAsync($"> I didnt find this character in your character list, try calling it by name?");
+                return;
+            }
+
+            if(matchingCharacter.SellPrice <= 0)
+            {
+                await context.RespondAsync($"> Cant sacrifice this character or sacrifice price is zero");
+                return;
+            }
+
+            if (matchingCharacter == user.EquippedCharacter) SetCharacterAction.Dequip(user);
+            user.Characters.Remove(matchingCharacter);
+
+            user.AddCurrency(new DiscordWallet(0, 0, matchingCharacter.SellPrice));
+
+            var updateQuery = new QueryBuilder(user, QueryElement.EquippedCharacter, QueryElement.Characters, QueryElement.Wallet).BuildUpdateQuery();
+            await DataHandler.SendData(user, updateQuery);
+
+            await context.RespondAsync($"{user.Username} sacrificed {matchingCharacter.CharacterName} for {matchingCharacter.SellPrice} 💊");
+        }
+
+        [Command("sacrifice"), Description("Sacrifice character for some pills")]
+        public async Task Sacrifice(CommandContext context, string name) => await Sacrifice(context, name, string.Empty);
+    }
+}
