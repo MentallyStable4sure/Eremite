8ae2f09b3aa61cfdff2068d5b34540b7c4565626
dental misorder <denys.bondarenko@nure.ua>
2023-06-22 17:38:51 +0300

    Added new adventures 🎲🍂

--- Eremite/Data/DiscordData/Region.cs
+++ Eremite/Data/DiscordData/Region.cs
@@ -4,16 +4,17 @@ namespace Eremite.Data.DiscordData
     public enum Region
     {
         None = 0,
-        Monstadt,
-        Inazuma,
-        Liyue,
-        Chasm,
-        Sumeru,
-        Fontaine,
-        Natlan,
-        Snezhnaya,
-        Khaenriah,
-        Dragonspine,
-        Enkanomiya
+        Monstadt = 1,
+        Inazuma = 2,
+        Liyue = 3,
+        Chasm = 4,
+        Sumeru = 5,
+        Fontaine = 6,
+        Natlan = 7,
+        Snezhnaya = 8,
+        Khaenriah = 9,
+        Dragonspine = 10,
+        Enkanomiya = 11,
+        Watatsumi = 12
     }
 }
