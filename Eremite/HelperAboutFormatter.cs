791eb054b2688431a297d2231526fa03507afbe0
dental misorder <denys.bondarenko@nure.ua>
2023-07-12 16:35:13 +0300

    !help command updated

---
+++ Eremite/HelperAboutFormatter.cs
@@ -0,0 +1,20 @@
+﻿
+using Eremite.Actions;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Entities;
+using DSharpPlus.CommandsNext.Converters;
+
+namespace Eremite
+{
+    internal class HelperAboutFormatter : DefaultHelpFormatter
+    {
+        public HelperAboutFormatter(CommandContext ctx) : base(ctx)
+        {
+        }
+
+        public override CommandHelpMessage Build()
+        {
+            return new CommandHelpMessage(embed: HelpAction.GetEmbed().Build());
+        }
+    }
+}
